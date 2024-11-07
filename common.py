43dfa329284163f78345ce680103a6f5a6195822
mohankumargupta <mohankumargupta@users.noreply.github.com>
2024-11-07 20:11:06 +1100

    remove hardcoded filename

--- common.py
+++ common.py
@@ -13,7 +13,7 @@ def save_as_scad(part, name):
 
 
 def save_as_stl(name):
-    subprocess.call([openscad_location, "-o", f"{name}.stl", "{name}.scad"])
+    subprocess.call([openscad_location, "-o", f"{name}.stl", f"{name}.scad"])
 
 def save_as_scad_and_stl(part):
     root = root_name()
